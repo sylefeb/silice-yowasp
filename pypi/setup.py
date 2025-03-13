a4a0c84fd2feeda6526f48f588e75bf936c21bb0
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2025-03-13 22:03:41 +0100

    tweak: packaging info

--- pypi/setup.py
+++ pypi/setup.py
@@ -3,10 +3,15 @@ from setuptools_scm.git import parse as parse_git
 import subprocess
 import os
 
+def commit_count(path):
+  repo_path    = os.path.abspath(path)
+  commit_count = subprocess.check_output(["git", "rev-list", "--count", "HEAD"], cwd=repo_path).strip().decode("utf-8")
+  return commit_count
+
 def version():
   # for now, a straight-forward version string the increases with commit count
-  repo_path = os.path.abspath("../silice-src")
-  commit_count = subprocess.check_output(["git", "rev-list", "--count", "HEAD"], cwd=repo_path).strip().decode("utf-8")
-  return f"1.0.post{commit_count}"
+  count1 = commit_count("../silice-src")
+  count2 = commit_count("..")
+  return f"1.0.post{count1}{count2}"
 
 setup(version=version())
