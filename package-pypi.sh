79a08be14584383f3c7c398ab39ae704454b5e99
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2025-03-13 20:29:03 +0100

    fix: version string (preliminary)

--- package-pypi.sh
+++ package-pypi.sh
@@ -4,6 +4,7 @@ PYTHON=${PYTHON:-python}
 
 rm -rf pypi/yowasp_silice/share
 
+cp README.md pypi/README.md
 cp silice-prefix/bin/silice silice-prefix/bin/silice.wasm
 cp silice-src/bin/silice-make.py silice-prefix/bin/silice_make.py
 cp silice-src/bin/report-cycles.py silice-prefix/bin/report_cycles.py
--- pypi/setup.py
+++ pypi/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 from setuptools_scm.git import parse as parse_git
+import subprocess
+import os
 
 def version():
-    return "0.1"
+  # for now, a straight-forward version string the increases with commit count
+  repo_path = os.path.abspath("../silice-src")
+  commit_count = subprocess.check_output(["git", "rev-list", "--count", "HEAD"], cwd=repo_path).strip().decode("utf-8")
+  return f"1.0.post{commit_count}"
 
-setup(
-    version=version(),
-)
+setup(version=version())
